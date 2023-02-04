4988541c874ba8c1322da5072efbab2a82c0890f
Guilherme Gabriel <guilhermag@gmail.com>
2023-02-04 15:21:01 -0300

    finalizacao testes DialogComponent

--- src/app/shared/dialog/dialog.component.spec.ts
+++ src/app/shared/dialog/dialog.component.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 import { DialogComponent } from './dialog.component';
 
@@ -8,9 +14,13 @@ describe('DialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DialogComponent ]
-    })
-    .compileComponents();
+      declarations: [DialogComponent],
+      imports: [MatIconModule, MatDialogModule],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: [] },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DialogComponent);
     component = fixture.componentInstance;
